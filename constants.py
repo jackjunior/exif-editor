bab9e8f238e3599b032fa5de45ce6ef9f43334e2
Jack Lam <jack.lam@shoplineapp.com>
2024-08-01 17:22:00 -0400

    feat(helper) - Add new helper function and code refactor
    1. Code refactor in app.py and helpers.py
    2. Add constants.py to hold all constant variable
    3. Add new function (validate_datetime, clear_console)

---
+++ constants.py
@@ -0,0 +1,13 @@
+FOLDER_PATH = 'sample_images'
+
+MENU_OPTIONS = [
+   '1. Show Image EXIF',
+   '2. Update Image Data',
+   '3. Option 3',
+   '4. Exit'
+]
+
+UPDATE_MENU_OPTIONS = [
+      '1. Image Date',
+      '2. Exit'
+]
